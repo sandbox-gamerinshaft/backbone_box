9cac26388bdeed171d421ca8ea9c98e9e1e21b91
j.taneichi <j.tane3ma@gmail.com>
2014-09-11 18:33:35 +0900

    application pageを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :boxes
+  resources :boxes do
+    member do
+      get "markdown"
+    end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +60,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root "application#index"
 end
